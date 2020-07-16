844aa6e796c5925046e02a7d251e190cbe9a5922
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-16 22:37:33 +0530

    use case 8 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -22,6 +22,7 @@ TotalHour=0
 TotalDays=0
 wage=0
 present=1
+i=0
 while [ $TotalHour -le 100 ] && [ $TotalDays -le 20 ]
 do
 attendance=$((RANDOM%3))
@@ -29,14 +30,21 @@ case "$attendance" in
 1)
 	TotalDays=$(($TotalDays+1))
 	workHour=$( workhour 1)
+	daily[$i]=$(($workHour*$WagePerHour))
+	i=$(($i+1))
 	TotalHour=$(($TotalHour+$workHour))
 	;;
 2)
 	TotalDays=$(($TotalDays+1))
 	workHour=$( workhour 2)
+	daily[$i]=$(($workHour*$WagePerHour))
+        i=$(($i+1))
         TotalHour=$(($TotalHour+$workHour))
 	;;
 0)
+	workHour=$( workhour 0)
+	daily[$i]=$(($workHour*$WagePerHour))
+        i=$(($i+1))
 	TotalDays=$(($TotalDays+1))
 	;;
 *)
@@ -44,6 +52,7 @@ case "$attendance" in
 	;;
 esac
 done
+echo "daily wage" ${daily[@]}
 if [ $TotalHour -ge 100 ]
 then
 	wage=$(($WagePerHour*$TotalHour))
