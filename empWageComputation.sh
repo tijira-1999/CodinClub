159c7b56ca56cfd67d818f741473dc8a2706f086
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-16 22:46:06 +0530

    Use case 9 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -14,6 +14,8 @@ function workhour() {
 }
 echo "Welcome to employee wage computation program"
 
+declare -A daily
+
 WagePerHour=20
 FullDayHour=8
 ParttimeHour=4
@@ -22,28 +24,28 @@ TotalHour=0
 TotalDays=0
 wage=0
 present=1
-i=0
-while [ $TotalHour -le 100 ] && [ $TotalDays -le 20 ]
+i=1
+while [ $TotalHour -ne 100 ] && [ $TotalDays -ne 20 ]
 do
 attendance=$((RANDOM%3))
 case "$attendance" in
 1)
 	TotalDays=$(($TotalDays+1))
 	workHour=$( workhour 1)
-	daily[$i]=$(($workHour*$WagePerHour))
+	daily[Day$i]=$(($workHour*$WagePerHour))
 	i=$(($i+1))
 	TotalHour=$(($TotalHour+$workHour))
 	;;
 2)
 	TotalDays=$(($TotalDays+1))
 	workHour=$( workhour 2)
-	daily[$i]=$(($workHour*$WagePerHour))
+	daily[Day$i]=$(($workHour*$WagePerHour))
         i=$(($i+1))
         TotalHour=$(($TotalHour+$workHour))
 	;;
 0)
 	workHour=$( workhour 0)
-	daily[$i]=$(($workHour*$WagePerHour))
+	daily[Day$i]=$(($workHour*$WagePerHour))
         i=$(($i+1))
 	TotalDays=$(($TotalDays+1))
 	;;
@@ -52,7 +54,11 @@ case "$attendance" in
 	;;
 esac
 done
-echo "daily wage" ${daily[@]}
+for key in ${!daily[@]}
+do
+	echo $key: ${daily[$key]}
+done
+
 if [ $TotalHour -ge 100 ]
 then
 	wage=$(($WagePerHour*$TotalHour))
