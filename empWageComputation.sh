68358a3793bbe71ef0fee19852272d648e3d0c70
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-16 20:40:33 +0530

    use case 6 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,26 +5,35 @@ WagePerHour=20
 FullDayHour=8
 ParttimeHour=4
 WorkingDayPerMonth=20
+TotalHour=0
+TotalDays=0
 wage=0
 present=1
+while [ $TotalHour -le 100 ] && [ $TotalDays -le 20 ]
+do
 attendance=$((RANDOM%3))
 case "$attendance" in
 1)
-	wage=$(($WagePerHour*$FullDayHour*$WorkingDayPerMonth))
-	echo "Employee is present"
-	echo "Monthly wage is " $wage
+	TotalDays=$(($TotalDays+1))
+	TotalHour=$(($TotalHour+8))
 	;;
 2)
-	wage=$(($WagePerHour*$ParttimeHour*$WorkingDayPerMonth))
-        echo "Employee is present (PART TIME)"
-        echo "Monthly wage is " $wage
+	TotalDays=$(($TotalDays+1))
+        TotalHour=$(($TotalHour+4))
 	;;
 0)
-	echo "Employee is absent"
-	echo "MOnthly wage is " $wage
+	TotalDays=$(($TotalDays+1))
 	;;
 *)
 	echo "Invalid"
 	;;
 esac
-
+done
+if [ $TotalHour -ge 100 ]
+then
+	wage=$(($WagePerHour*$TotalHour))
+	echo "Monthly wage is " $wage
+else
+	wage=$(($WagePerHour*$TotalHour))
+        echo "Monthly wage is " $wage
+fi
