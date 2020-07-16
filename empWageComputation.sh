c4c7dcc030bbded0a081e9f97c48c7b3d857eeed
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-16 19:04:20 +0530

    use case 3 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,20 @@ echo "Welcome to employee wage computation program"
 
 WagePerHour=20
 FullDayHour=8
+ParttimeHour=4
 wage=0
 present=1
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 if [ $attendance -eq 1 ]
 then
 	wage=$(($WagePerHour*$FullDayHour))
 	echo "Employee is present"
 	echo "Daily wage is " $wage
+elif [ $attendance -eq 2 ]
+then
+	wage=$(($WagePerHour*$ParttimeHour))
+        echo "Employee is present (PART TIME)"
+        echo "Daily wage is " $wage
 else
 	echo "Employee is absent"
 	echo "Daily wage is " $wage
