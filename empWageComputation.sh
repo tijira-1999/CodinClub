ea016129f8ec1cab36564c398b12d1701e1a66c9
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-16 18:55:47 +0530

    Use case 2 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 echo "Welcome to employee wage computation program"
+
+WagePerHour=20
+FullDayHour=8
+wage=0
 present=1
 attendance=$((RANDOM%2))
 if [ $attendance -eq 1 ]
 then
+	wage=$(($WagePerHour*$FullDayHour))
 	echo "Employee is present"
+	echo "Daily wage is " $wage
 else
 	echo "Employee is absent"
+	echo "Daily wage is " $wage
 fi
