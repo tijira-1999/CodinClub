e26ad67eb1addd9d335e91625045d5037ac8354c
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-16 18:44:19 +0530

    Use case 1 completed

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "Welcome to employee wage computation program"
-
+present=1
+attendance=$((RANDOM%2))
+if [ $attendance -eq 1 ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
